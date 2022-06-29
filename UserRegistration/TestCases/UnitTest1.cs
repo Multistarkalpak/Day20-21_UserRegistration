d8af10a102c1a391ae37f30db1727b6cba249a8f
Multistarkalpak <105732959+Multistarkalpak@users.noreply.github.com>
2022-06-29 22:52:25 +0530

    [kalpak] Adding Password Validation

--- UserRegistration/TestCases/UnitTest1.cs
+++ UserRegistration/TestCases/UnitTest1.cs
@@ -1,5 +1,7 @@
 using UserRegistrationTestAndRegex;
 
+using System.Text.RegularExpressions;
+
 namespace UserRegistrationTest
 {
     public class Tests
@@ -44,7 +46,7 @@ namespace UserRegistrationTest
         {
             // Arrange 
             bool expected = true;
-            string email = "kalpakc28@gmail.com";
+            string email = "kalpac28@gmail.com";
             UserRegistrationDetails user = new UserRegistrationDetails();
 
             // Act
@@ -58,7 +60,7 @@ namespace UserRegistrationTest
         {
             // Arrange 
             bool expected = true;
-            string mobile = "91 7768757370";
+            string mobile = "91 8618199771";
             UserRegistrationDetails user = new UserRegistrationDetails();
 
             // Act
@@ -67,7 +69,23 @@ namespace UserRegistrationTest
             // Assert
             Assert.AreEqual(expected, result);
         }
+        [Test]
+        public void Password()
+        {
+            // Arrange 
+            bool expected = true;
+            string password = "abcdfghihgE1@";
+            UserRegistrationDetails user = new UserRegistrationDetails();
 
+            // Act
+            bool result = user.ValidatePassword(password);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
 
     }
+
+
+
 }
